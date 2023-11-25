b1461d92186ef47e62b6ae0a7ca1ca652f530656
fadli <fadlirahmat0623@gmail.com>
2023-11-26 00:28:33 +0700

    membuat branch homepage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,7 @@ import '../styles/main.css';
 
 const container = document.querySelector('body');
 container.innerHTML = `
-    <h1 class="text-3xl font-bold underline">
+    <h1 class="text-3xl font-bold underline ">
         NyokLapor
     </h1>
 `;
