8086358832c39cfb2c6de0f76e252d05b6881d7f
Hazron <hazron2003redian@gmail.com>
2023-12-01 20:39:48 +0700

    Selesai Form : Penambahan Koordinat

--- src/scripts/views/pages/laporan.js
+++ src/scripts/views/pages/laporan.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-empty-function */
 /* eslint-disable quotes */
 const Laporan = {
   async render() {
@@ -21,17 +23,17 @@ const Laporan = {
             <input type="date" placeholder="Tanggal Kejadian * " id="tanggal_kejadian" name="tanggal_kejadian" class="text-black text-lg h-[74px] font-normal font-Poppins border-2 border-black rounded-sm px-2 py-1 w-full lg:max-w-[800px] mb-2">
             
             <button id="lokasi_kejadian" class="text-black text-lg h-[74px] font-normal font-Poppins border-2 border-black rounded-sm px-2 py-1 w-full lg:max-w-[800px] mb-2 transition duration-300 ease-in-out hover:bg-gray-200 hover:border-gray-400" type="button">
-            Lokasi Kejadian *
-            </button>
+            Tampilkan Lokasi (Latitude & Longitude) *
+          </button>
                      
             <input type="file" class="text-black text-2xl font-bold font-['Poppins'] h-[74px] font-normal font-Poppins border-2 border-black rounded-sm px-2 py-1 w-full lg:max-w-[800px] mb-2">
             <div class="text-black text-base font-normal font-['Poppins']">Upload Lampiran (Max 2 MB)</div>
             
             <button id="submitBtn" class="lg:w-96 h-16 px-36 pt-5 pb-4 bg-pink-900 rounded-2xl shadow justify-start items-center inline-flex mt-4">
-            <div class="text-center text-white text-xl font-bold font-['Poppins']">NYOKLAPOR!</div>
-          </button>
+              <div class="text-center text-white text-xl font-bold font-['Poppins']" id="laporButton">NYOKLAPOR!</div>
+            </button>
             <div id="confirmationPopup" class="w-96 h-60 relative bg-gray-800" style="display: none;">
-            <div class="left-[118px] top-[61px] absolute text-stone-300 text-xl font-bold font-['Poppins']">Apakah kamu yakin ?</div>
+              <div class="left-[118px] top-[61px] absolute text-stone-300 text-xl font-bold font-['Poppins']">Apakah kamu yakin ?</div>
               <div class="w-96 h-px left-0 top-[132px] absolute border-2 border-black border-opacity-90"></div>
               <div class="px-5 pt-1.5 pb-2 left-[138px] top-[160px] absolute bg-pink-900 bg-opacity-90 rounded-lg justify-center items-center inline-flex">
                 <div class="text-stone-300 text-base font-bold font-['Poppins']">TIDAK</div>
@@ -44,36 +46,38 @@ const Laporan = {
         </div>
       </div>
     </div>
+    <div class="ml-[45px] mt-[40px] text-black text-5xl font-bold font-['Poppins']">IMPORT PENGGUNA</div>
+    
   </section>
-  
-  
-  
     `;
   },
 
   async afterRender() {
-    document.addEventListener("DOMContentLoaded", () => {
-      const submitBtn = document.getElementById('submitBtn');
-      const popup = document.getElementById('confirmationPopup');
+    const locationButton = document.getElementById('lokasi_kejadian');
 
-      // Menambahkan event listener saat tombol ditekan
-      submitBtn.addEventListener('click', () => {
-        // Menampilkan popup
-        popup.style.display = 'block';
-      });
-
-      // Menambahkan event listener untuk tombol 'IYA'
-      document.querySelector('#confirmationPopup .bg-white').addEventListener('click', () => {
-        // Menyembunyikan popup saat 'IYA' ditekan
-        popup.style.display = 'none';
-      });
-
-      // Menambahkan event listener untuk tombol 'TIDAK'
-      document.querySelector('#confirmationPopup .bg-pink-900').addEventListener('click', () => {
-        // Menyembunyikan popup saat 'TIDAK' ditekan
-        popup.style.display = 'none';
-      });
+    locationButton.addEventListener('click', () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        locationButton.textContent = 'Geolocation is not supported by this browser.';
+      }
     });
+
+    function showPosition(position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      const mapLink = `https://www.google.com/maps?q=${latitude},${longitude}`;
+      const mapAnchor = document.createElement('a');
+      mapAnchor.href = mapLink;
+      mapAnchor.target = '_blank';
+      mapAnchor.textContent = `Latitude: ${latitude}, Longitude: ${longitude} (Lihat di Maps)`;
+
+      locationButton.innerHTML = '';
+      locationButton.appendChild(mapAnchor);
+
+      // Lakukan apa pun dengan nilai latitude dan longitude yang Anda dapatkan di sini
+    }
   },
 };
 
