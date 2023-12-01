d88d0aa2854ba213c32cbeaba34ddb8cd9f970dc
Hazron <hazron2003redian@gmail.com>
2023-12-01 16:18:25 +0700

    Commit Page Lapor

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,10 @@
+import Home from '../views/pages/home';
+import Laporan from '../views/pages/laporan';
+
+const routes = {
+  '/': Home, // default page
+  '/home': Home,
+  '/laporan': Laporan,
+};
+
+export default routes;
