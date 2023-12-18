dca4ea5e96eff7e41c8c0f1312083c0791d5fe81
Mohamad Ripal Perdiansyah <94633492+mripalp@users.noreply.github.com>
2023-12-18 11:05:43 +0700

    Perubahan Output Directory

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     clean: true,
   },
   module: {
@@ -37,7 +37,7 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/assets'),
+          to: path.resolve(__dirname, 'public/assets'),
         },
       ],
     }),
